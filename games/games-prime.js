47b15299753d88b15e77606bf76d4e2f0106cbb9
Anton <karmeowwoof@gmail.com>
2021-12-01 06:54:07 +0300

    fix ptoblem with games-prime

--- games/games-prime.js
+++ games/games-prime.js
@@ -4,6 +4,9 @@ import gameTemplate from '../src/index.js';
 const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isNumberPrime = (number) => {
+  if (num < 2) {
+    return false;
+  }
   for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0 && number !== i) {
       return false;
