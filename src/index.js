5a07c992718da2c052a3f21f419d4dd168f4a8fa
Anton <karmeowwoof@gmail.com>
2021-12-25 05:16:15 +0300

    delete quote

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const launchGameLogic = (gameRules, giveData) => {
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer !== correctAnswer.toString()) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, '${userName}'!`);
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
     console.log('Correct!');
