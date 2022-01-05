905c0ce1376ab696d93ef04bcc63d3f3633774b4
Anton <karmeowwoof@gmail.com>
2022-01-05 16:01:56 +0300

    remove comment

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,16 +3,6 @@ import { launchGameLogic, roundCount } from '../index.js';
 
 const gameDescription = 'What number is missing in the progression?';
 
-const minCount = 0;
-const maxCount = 10;
-
-/* Предыдущий комментарий на эту тему:
- https://github.com/karmeowwoof/frontend-project-lvl1/blob/817f43def25bb4495d702049ca667a6d65667568/src/games/games-progression.js#L25-L26 Тут также магические числа. Потому лучше их заменить.
- Еще возник вопрос по поводу переписывания логики игр: после исправления в каждой игре возникли
- схожие блоки кода (code climate недоволен).
- Принцип разделения ответственности соблюдается, но
- принцип DRY - нет.Так и должно быть? */
-
 const arithmeticProgressionLength = 10;
 
 const createProgression = (number, difference) => {
@@ -25,8 +15,8 @@ const createProgression = (number, difference) => {
 
 const makeRounds = () => {
   const startNumber = getRandomNumber();
-  const hiddenIndex = getRandomNumber(minCount, maxCount);
-  const difference = getRandomNumber(minCount, maxCount);
+  const hiddenIndex = getRandomNumber(0, 10);
+  const difference = getRandomNumber(0, 10);
   const progression = createProgression(startNumber, difference);
   progression[hiddenIndex] = '..';
   const question = progression.join(' ');
